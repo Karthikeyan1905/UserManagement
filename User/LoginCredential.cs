0f3ff80864f94c99d4035b159cde7cf8a8d15c5c
Your Name <your-email@example.com>
2025-07-11 21:31:38 +0530

    changes

---
+++ User/LoginCredential.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserManagement.User
+{
+    public class LoginCredential
+    {
+        
+        //public long LoginID { get; set; }
+        public string? loginUsername {  get; set; }
+        private string _loginPassword = "1234";
+        public string? loginPassword
+        {
+            get { return _loginPassword; }
+            set { _loginPassword = value ?? "1234"; }
+        }
+        //public string? status { get; set; }
+
+    }
+}
--- User/User.cs
+++ User/User.cs
@@ -10,9 +10,9 @@ namespace UserManagement.User
 {
     public class UserInfo
     {
-        internal object departments;
-        internal object Experiance;
-
+        public object departments;
+        public object Experiance;
+        public LoginCredential? loginCredential { get; set; }
         public int? userID { set; get; }
         public string? email { set; get; }
         public string? userName { set; get; }
@@ -42,15 +42,10 @@ namespace UserManagement.User
             }
         }
         public bool HasPassport { get; set; }
-
+         
         public List<Phone> phone { get; set; }
 
-        public List<UserManagement.Department.Department> department { get; set; }
-        public List<UserManagement.Department.Designation> designation { get; set; }
-        public List<UserManagement.Department.Employee> employee { get; set; }
-        public List<UserManagement.Team.Team> team { get; set; }
-
-        public List<UserManagement.Team.TeamUser> teamUser { get; set; }
+        
 
 
         public List<Education> educations { get; set; }
@@ -59,5 +54,11 @@ namespace UserManagement.User
         public bool HasExperience { get; set; }
         public List<Company> experiance { get; set; }
         public static List<UserInfo> RegisteredUsers { get; } = new();
+        public List<UserManagement.Department.Department> department { get; set; }
+        public List<UserManagement.Department.Designation> designation { get; set; }
+        public List<UserManagement.Department.Employee> employee { get; set; }
+        public List<UserManagement.Team.Team> team { get; set; }
+
+        public List<UserManagement.Team.TeamUser> teamUser { get; set; }
     }
 }
